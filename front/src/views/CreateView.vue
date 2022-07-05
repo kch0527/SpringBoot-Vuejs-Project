0bff07842183b58a6b0a4b782ca5df7a7bef7a6e
chan <jhgu128@naver.com>
2022-07-05 12:35:14 +0900

    2022.07.05 front-등록폼, CORS 문제 해결

---
+++ front/src/views/CreateView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+import axios from 'axios';
+
+const title = ref("")
+const content = ref("")
+
+const create = function (){
+  axios.post("/api/goods",{
+    title : title.value,
+    content : content.value
+  });
+}
+</script>
+
+<template>
+  <div>
+   <el-input v-model="title" type="text" placeholder="상품명을 입력해주세요."/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" placeholder="상품정보를 입력해주세요"/>
+  </div>
+
+  <div>
+    <el-button type="primary" @click="create()">상품 등록</el-button>
+  </div>
+
+</template>
+
+<style>
+
+</style>
