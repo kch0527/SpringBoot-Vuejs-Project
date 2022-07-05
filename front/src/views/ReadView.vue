1e8ace9891c3a3ef7581d8b29ac8d0d66b83b820
chan <jhgu128@naver.com>
2022-07-05 15:53:47 +0900

    2022.07.05 front-단일 조회 폼

---
+++ front/src/views/ReadView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {defineProps, onMounted, ref} from "vue";
+import axios from "axios";
+
+const props = defineProps({
+  goodsId: {
+    type :[Number, String],
+    require : true,
+  },
+});
+
+//추후 수정
+const goods = ref({
+  id: 0,
+  title: "",
+  content: "",
+});
+
+//추후 수정
+onMounted(() => {
+  axios.get(`/api/goods/${props.goodsId}`).then((response) => {
+   goods.value = response.data;
+  });
+})
+</script>
+
+<template>
+  <h2>{{ goods.title }}</h2>
+  <div>{{ goods.content }}</div>
+</template>
